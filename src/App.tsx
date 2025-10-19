deda2a593912885da01e33291548b54f3d5ce08b
marataitester-blip <marat.aitester@gmail.com>
2025-10-19 15:41:38 +0300

    Add main App component with routing and navigation

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { Routes, Route, Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import MainPage from './pages/MainPage'
+import Readings from './pages/Readings'
+import Encyclopedia from './pages/Encyclopedia'
+import Header from './components/Header'
+import './styles/vars.css'
+import './styles/global.css'
+
+export default function App() {
+  const { t } = useTranslation('common')
+  return (
+    <div className="app">
+      <Header />
+      <nav className="nav">
+        <Link to="/">{t('nav.home')}</Link>
+        <Link to="/readings">{t('nav.readings')}</Link>
+        <Link to="/encyclopedia">{t('nav.encyclopedia')}</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/readings" element={<Readings />} />
+        <Route path="/encyclopedia" element={<Encyclopedia />} />
+      </Routes>
+    </div>
+  )
+}
