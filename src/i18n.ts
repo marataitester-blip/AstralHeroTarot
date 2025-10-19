10cdf9d4167dd5203b57bdea345416a77e450ea9
marataitester-blip <marat.aitester@gmail.com>
2025-10-19 15:42:35 +0300

    Initialize i18n with backend and language detection

---
+++ src/i18n.ts
@@ -0,0 +1,22 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import HttpBackend from 'i18next-http-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
+
+i18n
+  .use(HttpBackend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    supportedLngs: ['en', 'ru'],
+    ns: ['common', 'cards'],
+    defaultNS: 'common',
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
+    interpolation: { escapeValue: false },
+    detection: { order: ['querystring', 'localStorage', 'navigator'], caches: ['localStorage'] },
+  })
+
+export default i18n
