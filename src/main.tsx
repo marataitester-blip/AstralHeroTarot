3ad33e7297d0127a31afa3b8fdd6ae930c90442f
marataitester-blip <marat.aitester@gmail.com>
2025-10-19 16:24:33 +0300

    Replace global styles with index.css import

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './styles/vars.css'
-import './styles/global.css'
+import './index.css'
 import './i18n'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
